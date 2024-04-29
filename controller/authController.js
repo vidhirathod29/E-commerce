4aae8963eeb7eafffc64a17ba2edea589aa23977
vidhirathod29 <vidhirathod.shivinfotech@gmail.com>
2024-04-29 18:11:13 +0530

    changes: applied space in controller

--- controller/authController.js
+++ controller/authController.js
@@ -9,12 +9,15 @@ module.exports = {
   registration: async (req, res, next) => {
     await registration(req, res, next);
   },
+
   login: async (req, res, next) => {
     await login(req, res, next);
   },
+  
   updateProfile: async (req, res, next) => {
     await updateProfile(req, res, next);
   },
+  
   viewProfile: async (req, res, next) => {
     await viewProfile(req, res, next);
   },
