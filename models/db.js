1749f51b59e5d2bb7d825a54f9befe3c411ae0cd
vidhirathod29 <vidhirathod.shivinfotech@gmail.com>
2024-07-27 12:53:55 +0530

    changes: added password

--- models/db.js
+++ models/db.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   process.env.DATABASE,
   process.env.USER_NAME,
-  '',
+  process.env.PASSWORD,
   {
     host: process.env.HOST,
     dialect: process.env.DIALECT,
